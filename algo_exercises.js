51334e35f5cd26c966d5e6cf52c51b1930aa5b4f
Mike Wong <wongmdesign@gmail.com>
2019-06-14 11:36:14 -0400

    new algo added finding diff in arrays

--- algo_exercises.js
+++ algo_exercises.js
@@ -693,3 +693,47 @@ let world = {
 
 console.log(searchParty('Francis', world))
 console.log(searchParty('Franco', world))
+
+function diffArray (arr1, arr2) {
+  var newObj = {}
+
+  helper(arr1)
+  helper(arr2)
+
+  let diffArr = []
+
+  // console.log(newObj)
+
+  for (let key in newObj) {
+    if (newObj[key] === 1) {
+      diffArr.push(key)
+    }
+  }
+  return diffArr
+
+  function helper (arr) {
+    for (let i = 0; i < arr.length; i++) {
+      if (newObj[arr[i]]) {
+        newObj[arr[i]]++
+      } else {
+        newObj[arr[i]] = 1
+      }
+    }
+  }
+}
+
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]))
+console.log(
+  diffArray(
+    ['diorite', 'andesite', 'grass', 'dirt', 'pink wool', 'dead shrub'],
+    ['diorite', 'andesite', 'grass', 'dirt', 'dead shrub']
+  )
+)
+console.log(
+  diffArray(
+    ['andesite', 'grass', 'dirt', 'pink wool', 'dead shrub'],
+    ['diorite', 'andesite', 'grass', 'dirt', 'dead shrub']
+  )
+)
+
+console.log(diffArray([1, 'calf', 3, 'piglet'], [1, 'calf', 3, 4]))
